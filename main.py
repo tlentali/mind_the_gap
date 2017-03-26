36d89cf208a2bd8135bd916cf7cb2b0af9b19482
roibos <thomas.lentali@gmail.com>
2017-03-26 23:51:44 +0200

    clean function added

--- main.py
+++ main.py
@@ -3,19 +3,23 @@ import requests
 from bs4 import BeautifulSoup
 import re
 
+def clean(text):
+    """
+        clean string
+    """
+    text = re.sub('^[^0-9]*', '', text)
+    text = re.sub('/\s\s+/g', ' ', text)
+    text = re.sub('s.*?d', 's d', text)
+    text = re.sub('\\.*$', '', text)
+    return text
+
 
 r = requests.get\
     ('https://www.infotbm.com/nextdeparture/B/stoparea/TBT5529/backward')
 
 c = r.content
 soup = BeautifulSoup(c, 'html.parser')
-
 samples = soup.find_all('span', attrs={"class": u"inline-left"})
 
 for tram in samples:
-    depart = tram.text
-    depart = re.sub('^[^0-9]*', '', depart)
-    depart = re.sub('/\s\s+/g', ' ', depart)
-    depart = re.sub('s.*?d', 's d', depart)
-    depart = re.sub('\\.*$', '', depart)
-    print(depart)
+    print(clean(tram.text))
