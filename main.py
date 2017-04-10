9f75ce636fa1549940b5ef0198143b57efe91f01
roibos <thomas.lentali@gmail.com>
2017-04-10 22:42:21 +0200

    neat result display

--- main.py
+++ main.py
@@ -2,24 +2,34 @@
 import requests
 from bs4 import BeautifulSoup
 import re
+import sys
+import time
+
 
 def clean(text):
     """
         clean string
     """
     text = re.sub('^[^0-9]*', '', text)
-    text = re.sub('/\s\s+/g', ' ', text)
     text = re.sub('s.*?d', 's d', text)
     text = re.sub('\\.*$', '', text)
+    # text = re.sub('/\s\s+/g', ' ', text)
+    text = re.sub('\*', ' ', text)
+    text = re.sub('\n', ' ', text)
+    text = re.sub('[ ]{2,}', ' ', text)
+
     return text
 
 
-r = requests.get\
-    ('https://www.infotbm.com/nextdeparture/B/stoparea/TBT5529/backward')
+while True:
+    r = requests.get\
+        ('https://www.infotbm.com/nextdeparture/B/stoparea/TBT5529/backward')
 
-c = r.content
-soup = BeautifulSoup(c, 'html.parser')
-samples = soup.find_all('span', attrs={"class": u"inline-left"})
+    c = r.content
+    soup = BeautifulSoup(c, 'html.parser')
+    samples = soup.find_all('span', attrs={"class": u"inline-left"})
 
-for tram in samples:
-    print(clean(tram.text))
+    for tram in samples:
+        print(clean(tram.text))
+    time.sleep(5)
+    print(50*"\n")
