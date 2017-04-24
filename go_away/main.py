5b8044e16b6bea2b505192fa6e8e82c5307ee6dd
roibos <thomas.lentali@gmail.com>
2017-04-24 08:51:05 +0200

    prod ready

--- go_away/main.py
+++ go_away/main.py
@@ -8,7 +8,7 @@ import time
 import utils
 import config
 
-"""
+
 while True:
     time.sleep(config.REFRESH)
     print(50*"\n")
@@ -17,27 +17,3 @@ while True:
     for tram in samples:
         print(utils.clean(tram.text))
     print(1*"\n")
-"""
-
-from tkinter import *
-
-fenetre = Tk()
-
-url = config.URL
-samples = utils.parserHtml(utils.getInfo(url))
-test = ""
-for tram in samples:
-    test = test + utils.clean(tram.text) + "\n"
-label = Label(fenetre, text=test)
-# label.pack()
-# fenetre.mainloop()
-
-while True:
-    time.sleep(5)
-    samples = utils.parserHtml(utils.getInfo(url))
-    test = ""
-    for tram in samples:
-        test = test + utils.clean(tram.text) + "\n"
-    label = Label(fenetre, text=test)
-    label.pack()
-    fenetre.update_idletasks()
