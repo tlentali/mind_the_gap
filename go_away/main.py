eaab0770d66b09461f7077f96549bc291a0d6cb6
tlentali <thomas.lentali@gmail.com>
2017-12-03 00:49:29 +0100

    exception ConnexionError added

--- go_away/main.py
+++ go_away/main.py
@@ -10,10 +10,15 @@ import config
 
 
 while True:
-    time.sleep(config.REFRESH)
-    print(50*"\n")
-    url = config.URL
-    samples = utils.parserHtml(utils.getInfo(url))
-    for tram in samples:
-        print(utils.clean(tram.text))
-    print(1*"\n")
+    try:
+        time.sleep(config.REFRESH)
+        print(50 * "\n")
+        url = config.URL
+        samples = utils.parserHtml(utils.getInfo(url))
+        for tram in samples:
+            print(utils.clean(tram.text))
+        print(1 * "\n")
+    except ConnectionError:
+        print(50 * "\n")
+        print("Tram doesn´t exist anymore!")
+        print(1 * "\n")
